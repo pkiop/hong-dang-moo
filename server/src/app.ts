c04538ec5de335f895e67bc9cf7885906c5ab2ee
pkiop <pkiopb@gmail.com>
2021-11-25 00:38:05 +0900

    fix port

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,12 +2,14 @@ import express from 'express';
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static('build'));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/build/index.html');
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('server is running!');
 });
